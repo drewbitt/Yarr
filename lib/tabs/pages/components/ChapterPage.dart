a1723f8557ff3b03528929ce040f6cd438470931
drewbitt <andrew@drewbitt.com>
2020-03-10 06:16:31 -0400

    Add chapter title to chapter page

--- lib/tabs/pages/components/ChapterPage.dart
+++ lib/tabs/pages/components/ChapterPage.dart
@@ -16,13 +16,24 @@ class ChapterPage extends StatelessWidget {
             final data = snapshot.data;
             return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   IconButton(
                     icon: Icon(Icons.arrow_back),
                     onPressed: () => Navigator.of(context).pop(),
                   ),
+                  Flexible(
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(6, 3, 6, 0),
+                          child: Text(data.title,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize:
+                                      MediaQuery.of(context).size.height / 43),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis))),
                   Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 4),
+                      padding: EdgeInsets.symmetric(horizontal: 6),
                       // useRichText fixes status screens, but is also ugly,
                       // just waiting on a fix from the library
                       child: Html(data: data.contents, useRichText: true))
