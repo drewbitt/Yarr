d212bcfa0882eca53c8d8712980d71fe94419ff6
drewbitt <andrew@drewbitt.com>
2020-03-15 05:02:17 -0400

    Cleanup home tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -37,64 +37,65 @@ class HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
 
   @override
   bool get wantKeepAlive => true;
-}
 
-_buildPageView(List<BookListResult> fictions, PageController pageController,
-        ValueNotifier currentPageNotifier) =>
-    PageView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: fictions.length,
-        controller: pageController,
-        itemBuilder: (context, index) {
-          return Padding(
-              padding: EdgeInsets.all(10), // not sure about value rn
-              child: InkWell(
-                  child: ResultCard(fictions[index], showBorder: false),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => NovelDetails(fictions[index])));
-                  }));
-        },
-        onPageChanged: (int index) {
-          currentPageNotifier.value = index;
-        });
-
-_trendingList() => _buildList(getTrendingFictions());
-
-_popularList() => _buildList(getWeeksPopularFictions());
-
-_buildList(func) {
-  final _pageController = PageController();
-  final _currentPageNotifier = ValueNotifier<int>(0);
-
-  return Container(
-      height: 160,
-      child: FutureBuilder<List<BookListResult>>(
-        future: func,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final data = snapshot.data;
-            return ArrowPageIndicator(
-                isInside: true,
-                pageController: _pageController,
-                currentPageNotifier: _currentPageNotifier,
-                itemCount: data.length,
-                child: _buildPageView(
-                    data, _pageController, _currentPageNotifier));
-          } else {
+  _buildPageView(List<BookListResult> fictions, PageController pageController,
+          ValueNotifier currentPageNotifier) =>
+      PageView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: fictions.length,
+          controller: pageController,
+          itemBuilder: (context, index) {
             return Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: 13,
-                    horizontal: MediaQuery.of(context).size.width / 40),
-                child: Row(
-                  children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.only(right: 5),
-                        child: CupertinoActivityIndicator()),
-                    Text("Getting chapters", style: TextStyle(fontSize: 15)),
-                  ],
-                ));
-          }
-        },
-      ));
+                padding: EdgeInsets.all(10), // not sure about value rn
+                child: InkWell(
+                    child: ResultCard(fictions[index], showBorder: false),
+                    onTap: () {
+//                    Navigator.of(context).push(MaterialPageRoute(
+//                        builder: (context) => NovelDetails(fictions[index])));
+                    }));
+          },
+          onPageChanged: (int index) {
+            currentPageNotifier.value = index;
+          });
+
+  _trendingList() => _buildList(getTrendingFictions());
+
+  _popularList() => _buildList(getWeeksPopularFictions());
+
+  _buildList(func) {
+    final _pageController = PageController();
+    final _currentPageNotifier = ValueNotifier<int>(0);
+
+    return Container(
+        height: 160,
+        child: FutureBuilder<List<BookListResult>>(
+          future: func,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final data = snapshot.data;
+              return ArrowPageIndicator(
+                  isInside: true,
+                  pageController: _pageController,
+                  currentPageNotifier: _currentPageNotifier,
+                  // not a fan that on black covers, the left icon disappears. Living with it right now
+                  itemCount: data.length,
+                  child: _buildPageView(
+                      data, _pageController, _currentPageNotifier));
+            } else {
+              return Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 13,
+                      horizontal: MediaQuery.of(context).size.width / 40),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: CupertinoActivityIndicator()),
+                      Text("Getting chapters", style: TextStyle(fontSize: 15)),
+                    ],
+                  ));
+            }
+          },
+        ));
+  }
 }
